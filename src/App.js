1fe157a7dd00c01143ba1d638191f6e2d64d5654
Mark Ngo <mark.ngo203@gmail.com>
2025-01-23 18:41:13 -0500

    Add routes to the different pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import Container from 'react-bootstrap/Container';
 import Header from './components/Header';
-import Body from './components/Body'
-import Posts from './components/Posts';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import FeedPage from './pages/FeedPage';
+import ExplorePage from './pages/ExplorePage';
+import LoginPage from './pages/LoginPage';
 
 export default function App() {
   return (
     <Container fluid className='App'>
-      <Header />
-      <Body sidebar>
-        <Posts />
-      </Body>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<FeedPage />} />
+          <Route path="/explore" element={<ExplorePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </BrowserRouter>
     </Container>
   );
 }
