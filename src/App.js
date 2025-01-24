aa8709426199f1550fe465d95fcb2dc3dba191ba
Mark Ngo <mark.ngo203@gmail.com>
2025-01-24 18:23:17 -0500

    Add user page and route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import FeedPage from './pages/FeedPage';
 import ExplorePage from './pages/ExplorePage';
+import UserPage from './pages/UserPage';
 import LoginPage from './pages/LoginPage';
 
 export default function App() {
@@ -13,6 +14,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<FeedPage />} />
           <Route path="/explore" element={<ExplorePage />} />
+          <Route path="/user/:username" element={<UserPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
